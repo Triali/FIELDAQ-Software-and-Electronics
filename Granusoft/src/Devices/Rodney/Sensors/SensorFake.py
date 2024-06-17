65006cd4b9679f75a66f7dba4cd65d8f7169a3d7
Triali <105738086+Triali@users.noreply.github.com>
2024-06-17 15:10:53 -0600

    Update SensorFake.py to be a singleton

--- Granusoft/src/Devices/Rodney/Sensors/SensorFake.py
+++ Granusoft/src/Devices/Rodney/Sensors/SensorFake.py
@@ -1,8 +1,9 @@
 import datetime
 from math import sin
 import math
+from util.SingletonClass import SingletonClass
 
-class Sensor:
+class Sensor(SingletonClass):
 
     def __init__(self):
         self.REAL_DATA = False
